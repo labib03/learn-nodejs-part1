75ca4eb9d4437322c2aea02023fc001f74e0b2a0
labib <ansorudinlabib1@gmail.com>
2022-11-09 14:29:14 +0700

    Status Code

--- server.js
+++ server.js
@@ -2,22 +2,24 @@ const http = require('http')
 
 const requestListener = (req, res) => {
     res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 200;
     const { method, url } = req;
-
+    
     if(url === '/'){
         switch (method) {
             case 'GET':
+                res.statusCode = 200;
                 res.end('<h1>Ini adalah halaman <b>Homepage</b></h1>.')
                 break;
         
             default:
+                res.statusCode = 400;
                 res.end(`<h1>Halaman tidak dapat diakses dengan method ${method}</h1>`)
                 break;
         }
 
-    }   else if (url === '/about') {
+    } else if (url === '/about') {
         if (method === 'GET') {
+            res.statusCode = 200;
           res.end('<h1>Halo! Ini adalah halaman about</h1>')
         } else if (method === 'POST') {
           let body = [];
@@ -29,11 +31,17 @@ const requestListener = (req, res) => {
           req.on('end', () => {
             body = Buffer.concat(body).toString();
             const {name} = JSON.parse(body);
+            res.statusCode = 200;
             res.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
           });
-      } else {
-        res.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
-      }
+        } else {
+          res.statusCode = 400;
+          res.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+        }
+    
+    } else {
+        res.statusCode = 404;
+        res.end('<h1>Halaman tidak ditemukan</h1>')
     }
  
 
