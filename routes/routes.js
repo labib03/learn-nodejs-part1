55188cbd4cc7b81875c8ad46024328c83e162b59
labib <ansorudinlabib1@gmail.com>
2022-11-09 17:04:33 +0700

    Path parameter

--- routes/routes.js
+++ routes/routes.js
@@ -28,6 +28,14 @@ const routes = [
     }
 },
 {
+    method: 'GET',
+    path: '/hello/{username?}',
+    handler: (request, h) => {
+        const { username = 'Stranger' } = request.params
+        return `Hello, ${username}.`
+    }
+},
+{
     method: '*',
     path: '/{any*}',
     handler: (request, h) => {
