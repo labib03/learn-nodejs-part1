458fdc04175e1bb1f944d286265b561e3c2e8bb9
labib <ansorudinlabib1@gmail.com>
2022-11-09 13:09:26 +0700

    Method/Verb Request

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const http = require('http')
 
 const requestListener = (req, res) => {
     res.setHeader('Content-Type', 'text/html');
-
     res.statusCode = 200;
-    res.end('<h1>Halo HTTP Server !</h1>')
+    const { method } = req;
+
+    if(method === 'GET'){
+        res.end(`Hallo, method yang anda gunakan adalah: ${method}`)
+    }
+
+    if(method === 'POST'){
+        res.end(`Hai, method yang anda gunakan adalah: ${method}`)
+    }
+
+    if(method === 'PUT'){
+        res.end(`Salam, method yang anda gunakan adalah: ${method}`)
+    }
+
+    if(method === 'DELETE'){
+        res.end(`Bonjour, method yang anda gunakan adalah: ${method}`)
+    }
+
+   
 }
 
 const server = http.createServer(requestListener);
