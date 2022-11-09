18b91af19549ffd258d7dfd25eaff9298425e5df
labib <ansorudinlabib1@gmail.com>
2022-11-09 14:20:35 +0700

    Routing Request

--- server.js
+++ server.js
@@ -3,32 +3,39 @@ const http = require('http')
 const requestListener = (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.statusCode = 200;
-    const { method } = req;
+    const { method, url } = req;
 
-    if(method === 'GET'){
-        res.end(`Hallo, method yang anda gunakan adalah: ${method}`)
-    }
+    if(url === '/'){
+        switch (method) {
+            case 'GET':
+                res.end('<h1>Ini adalah halaman <b>Homepage</b></h1>.')
+                break;
+        
+            default:
+                res.end(`<h1>Halaman tidak dapat diakses dengan method ${method}</h1>`)
+                break;
+        }
 
-    if(method === 'POST'){
-        let body = [];
-         req.on('data', (chunk) => {
-            body.push(chunk)
-        })
-        req.on('end', ()=>{
+    }   else if (url === '/about') {
+        if (method === 'GET') {
+          res.end('<h1>Halo! Ini adalah halaman about</h1>')
+        } else if (method === 'POST') {
+          let body = [];
+  
+          req.on('data', (chunk) => {
+            body.push(chunk);
+          });
+  
+          req.on('end', () => {
             body = Buffer.concat(body).toString();
-            const convertFromJson = JSON.parse(body);
-            const { name } = convertFromJson;
-            res.end(`Hai ${name}, method yang anda gunakan adalah: ${method}`) 
-         })
+            const {name} = JSON.parse(body);
+            res.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+          });
+      } else {
+        res.end(`<h1>Halaman tidak dapat diakses menggunakan ${method} request</h1>`);
+      }
     }
-
-    if(method === 'PUT'){
-        res.end(`Salam, method yang anda gunakan adalah: ${method}`)
-    }
-
-    if(method === 'DELETE'){
-        res.end(`Bonjour, method yang anda gunakan adalah: ${method}`)
-    }
+ 
 
    
 }
